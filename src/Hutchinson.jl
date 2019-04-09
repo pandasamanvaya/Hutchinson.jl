067de4177d95f6c2c4cad6c25ca7cf8acb8c8ba3
pandasamanvaya <pandasamanvaya@gmail.com>
2019-04-09 18:50:11 +0530

    updated Hutchinson.jl

--- src/Hutchinson.jl
+++ src/Hutchinson.jl
@@ -8,7 +8,7 @@ function inverse_mat_trace(A::AbstractMatrix)
 
 	H = hadamard(length(A[:,1]))
 	trace = 0
-	for i = 1 : length(H(1,:))
+	for i = 1 : length(H[1,:])
 		e = H[:,i]
 		trace += e'*Z*e
 	end
