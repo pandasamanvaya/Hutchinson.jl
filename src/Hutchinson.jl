ee68844afdc4a1158ece71b8d555cc013b3900ca
pandasamanvaya <pandasamanvaya@gmail.com>
2019-04-14 16:58:27 +0530

    updated Hutchinson.jl

--- src/Hutchinson.jl
+++ src/Hutchinson.jl
@@ -26,7 +26,7 @@ function random_vector(Z::AbstractMatrix)
 function inverse_mat_trace(A::AbstractMatrix)
 
 	Z = sparse_mat_inv(A)
-	if length(Z[:,1])%4 == 0 or length(Z[:,1]) == 2:
+	if length(Z[:,1])%4 == 0 || length(Z[:,1]) == 2
 		trace = hadamard_vector(Z)
 	else:
 		trace = random_vector(Z)
