c65ccdb7d8a6ea6ef2f201edcb4df3ef2a0ad9e1
pandasamanvaya <pandasamanvaya@gmail.com>
2019-04-14 17:59:43 +0530

    modified random_vectors

--- src/Hutchinson.jl
+++ src/Hutchinson.jl
@@ -3,37 +3,37 @@ using Hadamard
 export inverse_mat_trace
 include("sparse_mat_inv.jl")
 
-function hadamard_vector(Z::AbstractMatrix)
-	H = hadamard(length(Z[:,1]))
-	trace = 0
+#Generating random vectors
+function random_vector(Z::AbstractMatrix)
+	random = [-1 1]
+	vec = zeros(length(Z[:,1]), 10)
+
+	for i = 1 : length(Z[:,1])
+		for j = 1 : 10
+			vec[i][j] = random[rand(1:2)]
+		end
+	end
+
+	return vec
+end
+
+#Main Function
+function inverse_mat_trace(A::AbstractMatrix)
+
+	Z = sparse_mat_inv(A)
+
+	if length(Z[:,1])%4 == 0 || length(Z[:,1]) == 2
+		H = hadamard(length(Z[:,1])) #using hadamard vectors
+	else
+		H = random_vector(Z) #using random vectors
+	end
+	
 	for i = 1 : length(H[1,:])
 		e = H[:,i]
 		trace += e'*Z*e
 	end
+
 	return (trace/length(H[1,:]))
-end
-
-function random_vector(Z::AbstractMatrix)
-	random = [-1 1]
-	e = zeros(length(Z[:,1]))
-
-	for i = 1 : length(Z[:,1])
-		e[i] = random[rand(1:2)]
-	end
-
-	return e'*Z*e
-end
-
-function inverse_mat_trace(A::AbstractMatrix)
-
-	Z = sparse_mat_inv(A)
-	if length(Z[:,1])%4 == 0 || length(Z[:,1]) == 2
-		trace = hadamard_vector(Z)
-	else
-		trace = random_vector(Z)
-	end
-	
-	return trace
 end 
 
 end # module
