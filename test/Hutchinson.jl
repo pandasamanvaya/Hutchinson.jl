d9e74cd30ea1eb785fd5b90282b6a6acfeb3721d
pandasamanvaya <pandasamanvaya@gmail.com>
2019-04-14 21:21:36 +0530

    added test for Hutchinson.jl

--- test/Hutchinson.jl
+++ test/Hutchinson.jl
@@ -6,4 +6,7 @@ using Hutchinson
 @testset "trace of matrix inverse" begin
     A = sprand(7, 7, 0.2) + 7I
     @test abs(inverse_mat_trace(A) - tr(inv(Array(A)))) <= 0.01
+
+    A = sprand(12, 12, 0.4) + 12I
+    @test abs(inverse_mat_trace(A) - tr(inv(Array(A)))) <= 0.01
 end
